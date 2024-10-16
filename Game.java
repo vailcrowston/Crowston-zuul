b4a5f9ab599444ec125a82d6f0fa357e85808802
vailcrowston <vail.crowston@pcc.edu>
2024-10-15 21:18:21 -0700

    Exercise 8.16 - adding CommandWords class to Parser Class, Updated Game class, 8.15 was submitted in previous git commit. the 'napping' action is what was created.

--- Game.java
+++ Game.java
@@ -13,48 +13,11 @@ public class Game
 {
     private Parser parser;
     private Room currentRoom;
-        
-    /**
-     * Create the game and initialize its internal map.
-     */
+
     public Game() 
     {
         createRooms();
         parser = new Parser();
-    }
-
-    /**
-     * Create all the rooms and link their exits together.
-     */
-    private void createRooms()
-    {
-        Room makeupStudio, boutique, flowerGarden, spa, coffeeShop, fashionRunway;
-      
-        // create the rooms
-        makeupStudio = new Room("in a glamorous makeup studio.");
-        boutique = new Room("in a stylish boutique filled with the latest trends.");
-        flowerGarden = new Room("in a flower garden, bursting with colorful plants.");
-        spa = new Room("in a spa");
-        coffeeShop = new Room("in a cozy coffee shop.");
-        fashionRunway = new Room("on a fashion runway, lights flashing and music playing.");
-
-        // initialize room exits using the setExit method
-        makeupStudio.setExit("north", spa);
-        makeupStudio.setExit("east", boutique);
-        
-        boutique.setExit("west", makeupStudio);
-        
-        flowerGarden.setExit("east", spa);
-        flowerGarden.setExit("west", coffeeShop);
-        
-        spa.setExit("south", makeupStudio);
-        spa.setExit("north", flowerGarden);
-        
-        coffeeShop.setExit("east", flowerGarden);
-        
-        fashionRunway.setExit("south", coffeeShop);
-
-        currentRoom = makeupStudio;  // start game in the makeup studio
     }
 
     /**
@@ -82,9 +45,17 @@ public class Game
         System.out.println("In Girly World, you can explore fun and fabulous places.");
         System.out.println("Type 'help' if you need assistance.");
         System.out.println();
-        
-        // Print the starting location info
         printLocationInfo();
+    }
+
+    /**
+     * Print out some help information.
+     */
+    private void printHelp() 
+    {
+        System.out.println("You're fabulous! Don't worry, I got you covered.");
+        System.out.println("Your command words are:");
+        parser.showCommands();  // Delegates to the parser to show valid commands
     }
 
     /**
@@ -92,97 +63,9 @@ public class Game
      */
     private void printLocationInfo() 
     {
-        // Use the new getLongDescription method from the Room class
         System.out.println(currentRoom.getLongDescription());
     }
 
-    /**
-     * Given a command, process (that is: execute) the command.
-     * @param command The command to be processed.
-     * @return true If the command ends the game, false otherwise.
-     */
-     private boolean processCommand(Command command) 
-    {
-        boolean wantToQuit = false;
-
-        String commandWord = command.getCommandWord();
-
-        if (commandWord.equals("help")) {
-            printHelp();
-        }
-        else if (commandWord.equals("go")) {
-            goRoom(command);
-        }
-        else if (commandWord.equals("look")) {
-            look();  // Handle the look command
-        }
-        else if (commandWord.equals("quit")) {
-            wantToQuit = quit(command);
-        }
-        else {
-            System.out.println("I don't know what you mean, girly...");
-        }
-
-        return wantToQuit;
-    }
-
-
-    // User command implementations:
-
-    /**
-     * Print out some help information.
-     * Here we print a friendly message and a list of the 
-     * command words.
-     */
-    private void printHelp() 
-    {
-        System.out.println("You're fabulous! Don't worry, I got you covered.");
-        System.out.println();
-        System.out.println("Your command words are:");
-        System.out.println("   go quit help");
-    }
-
-    
-     private void look() 
-    {
-        System.out.println(currentRoom.getLongDescription());
-    }
-    
-    /**
-     * Try to go in one direction. If there is an exit, enter the new room,
-     * otherwise print an error message.
-     */
-    private void goRoom(Command command) 
-    {
-        if (!command.hasSecondWord()) {
-            System.out.println("Go where, Girly?");
-            return;
-        }
-
-        String direction = command.getSecondWord();
-        Room nextRoom = currentRoom.getExit(direction);
-
-        if (nextRoom == null) {
-            System.out.println("Oops! No way to go that way!");
-        } else {
-            currentRoom = nextRoom;
-            printLocationInfo();
-        }
-    }
-
-    /**
-     * "Quit" was entered. Check the rest of the command to see
-     * whether we really quit the game.
-     * @return true, if this command quits the game, false otherwise.
-     */
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
-            System.out.println("Quit what, Girly?");
-            return false;
-        }
-        else {
-            return true;  // signal that we want to quit
-        }
-    }
+    // Other methods...
 }
+
